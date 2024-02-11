04e9f3b3725cc44f3c037f81fa132f50cd8d1797
ahdbot <ahdbot3@gmail.com>
2024-02-11 20:55:13 +0300

    navbar end

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+require('bootstrap/dist/js/bootstrap.bundle');
+import "@fortawesome/fontawesome-free/js/all.js"
+
+import "jquery/dist/jquery.js";
+import "../sass/style.scss";
+
+console.log("Hello webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,118 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.(sass|css|scss)$/,
+
+        use: [
+
+          {
+
+            loader: MiniCssExtractPlugin.loader,
+
+            options: {
+
+              publicPath: '../'
+
+            }
+
+          },
+
+          "css-loader",
+
+          "sass-loader",
+
+        ],
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [{
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[ext]',
+      //       outputPath: "images",
+      //     }
+      //   },],
+      // },
+      // {
+      //   test: /\.(svg|eot|woff|woff2|ttf)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         name: '[name].[ext]',
+      //         outputPath: "fonts",
+      //       }
+      //     },
+      //   ],
+      // },
+
+      // {
+
+      //   test: /\.html$/i,
+
+      //   loader: 'html-loader',
+
+      // },
+
+
+      // {
+      //   test: require.resolve("jquery"),
+      //   loader: "expose-loader",
+      //   options: {
+      //     exposes: ["$", "jQuery"],
+      //   },
+      // },
+    ],
+  },
+  devServer: {
+
+    static: {
+
+      directory: path.join(__dirname, ' build'),
+
+    },
+
+    compress: true,
+
+    port: 9000,
+    hot: false,
+    open: true,
+
+    devMiddleware: {
+
+      writeToDisk: true,
+
+    },
+
+  },
+  entry: "./src/js/index.js",
+
+
+  output: {
+    publicPath: '/',
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
     }),
+    new MiniCssExtractPlugin({
+
+      filename: "css/style.css"
+    })
   ],
+
+
+
 }
